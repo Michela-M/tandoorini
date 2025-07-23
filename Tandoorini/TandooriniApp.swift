2c233e6efe7566eaf335584f34a3ad99735ea03c
Michela-M <michela_mullins@outlook.com>
2025-07-23 20:46:21 +0200

    Initial Commit

---
+++ Tandoorini/TandooriniApp.swift
@@ -0,0 +1,17 @@
+//
+//  TandooriniApp.swift
+//  Tandoorini
+//
+//  Created by Michela Mullins on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TandooriniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TandooriniTests/TandooriniTests.swift
@@ -0,0 +1,17 @@
+//
+//  TandooriniTests.swift
+//  TandooriniTests
+//
+//  Created by Michela Mullins on 23.07.2025.
+//
+
+import Testing
+@testable import Tandoorini
+
+struct TandooriniTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
